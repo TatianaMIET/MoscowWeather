4a30b569e93d33cb03987a5212c832323c7eb622
TatianaMIET <51916611+TatianaMIET@users.noreply.github.com>
2020-08-03 10:01:06 +0300

    Классы констант + Настройки в App.config
    Создание класса с константными заголовками столбцов в таблицах с архивами для проверки на возможность считывания файла.

---
+++ DataLayer/Codes/ErrorCodes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Codes
+{
+    public sealed class ErrorCodes
+    {
+        public static int SUCCESS { get; } = 0;
+        public static int INCORRECT_DOCUMENT { get; } = 1;
+    }
+}
---
+++ DataLayer/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DataLayer
+{
+    public sealed class Settings
+    {
+        public static string DATE { get; } = WebConfigurationManager.AppSettings["Date"];
+
+        public static string TIME { get; } = WebConfigurationManager.AppSettings["Time"];
+
+        public static string TEMPERATURE { get; } = WebConfigurationManager.AppSettings["Temperature"];
+
+        public static string HUMIDITY { get; } = WebConfigurationManager.AppSettings["Humidity"];
+
+        public static string DEW_POINT { get; } = WebConfigurationManager.AppSettings["DewPoint"];
+
+        public static string PRESSURE { get; } = WebConfigurationManager.AppSettings["Pressure"];
+
+        public static string WIND_DIRECTION { get; } = WebConfigurationManager.AppSettings["WindDirection"];
+
+        public static string WIND_SPEED { get; } = WebConfigurationManager.AppSettings["WindSpeed"];
+
+        public static string CLOUDINESS { get; } = WebConfigurationManager.AppSettings["Cloudiness"];
+
+        public static string CLOUD_BASE { get; } = WebConfigurationManager.AppSettings["CloudBase"];
+
+        public static string HORIZONTAL_VISIBILITY { get; } = WebConfigurationManager.AppSettings["HorizontalVisibility"];
+
+        public static string WEATHER_PHENOMENA { get; } = WebConfigurationManager.AppSettings["WeatherPhenomena"];
+    }
+}

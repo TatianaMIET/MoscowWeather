36ff538a6a971fcd99ad1bff5d337ee373de08cb
TatianaMIET <51916611+TatianaMIET@users.noreply.github.com>
2020-08-05 19:55:30 +0300

    Сервис для обработки входных данных и реализации логики

--- DataLayer/Services/WeatherService.cs
+++ DataLayer/Services/WeatherService.cs
@@ -1,135 +1,81 @@
 ﻿using DataLayer.Codes;
 using DataLayer.DBConnection;
 using DataLayer.Entity;
-using NPOI.SS.UserModel;
-using NPOI.XSSF.UserModel;
+using DataLayer.Model;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
-using System.Web.Profile;
 
 namespace DataLayer.Services
 {
     public class WeatherService : IDisposable
     {
         private readonly WeatherDBContext db;
+        private readonly ExelParseService exelParseService;
 
         public WeatherService()
         {
             db = new WeatherDBContext();
+            exelParseService = new ExelParseService();
         }
 
-        public int ReadExelArchives(List<HttpPostedFileBase> archives)
+        public List<string> ReadExelArchives(List<HttpPostedFileBase> archives)
         {
-            XSSFWorkbook xssfwb = new XSSFWorkbook(archives[0].InputStream);
-
-            ISheet sheet = xssfwb.GetSheetAt(0);
-
-            //TODO: вынести значения строк с заголовком в конфиг
-            IRow firstHeadingRow = sheet.GetRow(3);
-            IRow secondHeadingRow = sheet.GetRow(4);
-
-            //TODO: проверить условие
-            if (firstHeadingRow.LastCellNum == secondHeadingRow.LastCellNum)
+            List<string> result = new List<string>();
+            foreach (var archive in archives)
             {
-                List<string> heading = new List<string>(firstHeadingRow.LastCellNum);
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i <= firstHeadingRow.LastCellNum; i++)
-                {
-                    sb.Clear();
-                    sb.Append(firstHeadingRow.GetCell(i));
-                    sb.Append(" ");
-                    sb.Append(secondHeadingRow.GetCell(i));
-                    heading.Add(sb.ToString());
-                }
+                Dictionary<string, object> archiveWeatherData = exelParseService.ReadExelArchive(archive);
 
-                //Проверяем на соответствие столбцов порядку и содержнию заголовков архивов
-                bool isCorrect = true;
-                for (int i = 0; i <= heading.Count; i++)
+                if (archiveWeatherData.ContainsKey("result"))
                 {
-                    switch (i)
+                    if (archiveWeatherData["result"] != null)
                     {
-                        case 0:
-                            if (!heading[i].Equals(Settings.DATE))
-                                isCorrect = false;
-                            break;
-                        case 1:
-                            if (!heading[i].Equals(Settings.TIME))
-                                isCorrect = false;
-                            break;
-                        case 2:
-                            if (!heading[i].Equals(Settings.TEMPERATURE))
-                                isCorrect = false;
-                            break;
-                        case 3:
-                            if (!heading[i].Equals(Settings.HUMIDITY))
-                                isCorrect = false;
-                            break;
-                        case 4:
-                            if (!heading[i].Equals(Settings.DEW_POINT))
-                                isCorrect = false;
-                            break;
-                        case 5:
-                            if (!heading[i].Equals(Settings.PRESSURE))
-                                isCorrect = false;
-                            break;
-                        case 6:
-                            if (!heading[i].Equals(Settings.WIND_DIRECTION))
-                                isCorrect = false;
-                            break;
-                        case 7:
-                            if (!heading[i].Equals(Settings.WIND_SPEED))
-                                isCorrect = false;
-                            break;
-                        case 8:
-                            if (!heading[i].Equals(Settings.CLOUDINESS))
-                                isCorrect = false;
-                            break;
-                        case 9:
-                            if (!heading[i].Equals(Settings.CLOUD_BASE))
-                                isCorrect = false;
-                            break;
-                        case 10:
-                            if (!heading[i].Equals(Settings.HORIZONTAL_VISIBILITY))
-                                isCorrect = false;
-                            break;
-                        case 11:
-                            if (!heading[i].Equals(Settings.WEATHER_PHENOMENA))
-                                isCorrect = false;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    if (!isCorrect)
-                    {
-                        return ErrorCodes.INCORRECT_DOCUMENT;
+                        db.WeatherData.AddRange((List<Weather>)archiveWeatherData["result"]);
+                        db.SaveChangesAsync();
                     }
                 }
 
-
-                for (int row = 5; row <= sheet.LastRowNum; row++)
+                if (archiveWeatherData.ContainsKey("header_error"))
                 {
-                    IRow currentRow = sheet.GetRow(row);
-                    if (currentRow != null)
-                    {
-                        //запускаем цикл по столбцам
-                        for (int column = 0; column < 8; column++)
-                        {
-                            //получаем значение яейки
-                            var stringCellValue = currentRow.GetCell(column);
-                        }
-                    }
+                    result.Add(string.Format(ErrorTextTemplates.INCORRECT_HEADER, archive.FileName));
                 }
-                return ErrorCodes.SUCCESS;
+                if (archiveWeatherData.ContainsKey("parse_error"))
+                {
+                    result.Add(string.Format(ErrorTextTemplates.PARSE_ERROR, archive.FileName));
+                }
+                if (!(archiveWeatherData.ContainsKey("parse_error") || archiveWeatherData.ContainsKey("header_error")))
+                {
+                    result.Add(string.Format(ErrorTextTemplates.SUCCESS, archive.FileName));
+                }
+            }
+            return result;
+        }
+
+        public  WeatherModel GetWeatherData(int? yearFilter, int? monthFilter, int page, int pageSize)
+        {
+            IQueryable<Weather> source = db.WeatherData.OrderBy(p => p.Date);
+            var count = source.CountAsync();
+            PageModel pageModel = new PageModel(count.Result, page, pageSize);
+            if (yearFilter == null && monthFilter == null)
+            {
+                return new WeatherModel
+                {
+                    PageModel = pageModel,
+                    WeatherData = source.Skip((page - 1) * pageSize).Take(pageSize).ToList()
+                };
+
             }
             else
             {
-                return ErrorCodes.INCORRECT_DOCUMENT;
+                return new WeatherModel
+                {
+                    PageModel = pageModel,
+                    WeatherData = source.
+                    Where(w => w.Date.Year == yearFilter && w.Date.Month == (monthFilter ?? 1)).
+                    Skip((page - 1) * pageSize).Take(pageSize).ToList()
+                };
             }
         }
 

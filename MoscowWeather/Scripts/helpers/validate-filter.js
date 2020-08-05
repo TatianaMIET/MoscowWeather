859b7ed9688f02fb1afa87a69d2c85bd39b00e4e
TatianaMIET <51916611+TatianaMIET@users.noreply.github.com>
2020-08-05 19:54:10 +0300

    Валидирование инпутов с месяцем и годом для фильтрации

---
+++ MoscowWeather/Scripts/helpers/validate-filter.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    console.log("валидация");
+    $("#filter-date").validate({
+        rules: {
+            'month': {
+                digits: true,
+                number: true,
+                min: 1,
+                max: 12,
+                dependence: '#year'
+            },
+            'year': {
+                digits: true,
+                number: true,
+                min: 1950,
+                max: 2020 //TODO: текущая дата?
+            }
+        },
+        messages: {
+            'month': {
+                digits: "Только цифры",
+                number: "Только цифры"
+            },
+            'year': {
+                digits: "Только цифры",
+                number: "Только цифры"
+            }
+        }
+    })
+});
+
+$.validator.addMethod(
+    "dependence",
+    function (value, element, select) {
+        var el = $(select);
+        return this.optional(element) || el.val()!='';
+    },
+    "Введите год"
+);

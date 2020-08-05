64026bb4e096baccdcc45ef5deeb199783c2fff9
TatianaMIET <51916611+TatianaMIET@users.noreply.github.com>
2020-08-05 19:53:19 +0300

    Удаление не нужных для отслеживания файлов

--- MoscowWeather/Controllers/WeatherController.cs
+++ MoscowWeather/Controllers/WeatherController.cs
@@ -3,25 +3,53 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using AutoMapper;
 using DataLayer.DBConnection;
 using DataLayer.Entity;
+using DataLayer.Model;
 using DataLayer.Services;
+using MoscowWeather.Models;
 using NLog;
 
 namespace MoscowWeather.Controllers
 {
     public class WeatherController : Controller
     {
-        private readonly WeatherDBContext db = new WeatherDBContext();
         private readonly WeatherService weatherService = new WeatherService();
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         // GET: Weathers
-        public ActionResult Index()
+        public ActionResult GetWeatherData(int? year, int? month, int page=1)
         {
-            return View("Weather", db.WeatherData.ToList());
+            int pageSize = int.Parse(WebConfigurationManager.AppSettings["PageSize"]);
+
+            #region Mapper
+            var config = new MapperConfiguration(map =>
+            {
+                map.CreateMap<Weather, MoscowWeather.Models.WeatherModel>();
+                map.CreateMap<PageModel, PageVM>();
+                map.CreateMap<DataLayer.Model.WeatherModel, WeatherVM>()
+                .ForMember("PageViewModel", opt => opt.MapFrom(c =>  c.PageModel));
+            });
+            var mapper = new Mapper(config);
+            #endregion
+
+            DataLayer.Model.WeatherModel  weatherViewModel 
+                = weatherService.GetWeatherData(year, year==null ? null: month, page, pageSize);
+
+            DateVM dateViewModel = new DateVM
+            {
+                Year = year,
+                Month = month
+            };
+
+            WeatherVM weatherVM = mapper.Map<WeatherVM>(weatherViewModel);
+            weatherVM.DateViewModel = dateViewModel;
+
+            return View("Weather", weatherVM);
+            
         }
 
 
@@ -33,26 +61,15 @@ namespace MoscowWeather.Controllers
         [HttpPost]
         public ActionResult DownloadWeatherArchives(List<HttpPostedFileBase> archives)
         {
-           
-            weatherService.ReadExelArchives(archives);
-            return RedirectToAction("Index");
 
-        } 
+            ErrorMessagesModel model = new ErrorMessagesModel();
 
+            List<string> result = weatherService.ReadExelArchives(archives);
+            model.ErrorMessages = result;
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return PartialView("_DownloadResults", model);
+
         }
 
-        #region Данные
-        private readonly IMappingEngine mapping;
-
-        private ISettings settings;
-        #endregion
     }
 }
